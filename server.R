2446295ac32e4403af5970b89c85d97fa2a05878
fabmaccallini <fabrizio.maccallini@gmail.com>
2014-11-22 23:31:56 +0000

    Upload UI + Server

---
+++ server.R
@@ -0,0 +1,35 @@
+
+shinyServer(
+    function(input, output) {
+        output$MyPlot = renderPlot({
+            mns = NULL
+            for (i in 1 : input$sims)
+                mns = switch(input$var,
+                    "Binomial" =  c(mns, mean(rbinom(input$samp, 1, 0.5))),
+                    "Exponential" = c(mns, mean(rexp(input$samp))),
+                    "Gamma" = c(mns, mean(rgamma(input$samp, 1))),
+                    "Lognormal" = c(mns, mean(rlnorm(input$samp, 1))),
+                    "Normal" = c(mns, mean(rnorm(input$samp))),
+                    "Poisson" = c(mns, mean(rpois(input$samp, 1))),
+                    "Uniform" = c(mns, mean(runif(input$samp))))
+            mn = mean(mns)
+            sd = sd(mns)            
+            hist((mns - mn) / sd, prob = TRUE, breaks = 30, main = expression("Distribution of the sample means"),
+                xlab = "x", ylab = "Prob(x)", col = "lightblue")
+            if (input$norm) curve(dnorm(x, 0, 1), col = "red", add = TRUE, lwd = 3)
+            output$sd1 = renderPrint({
+                cint1 = mn + c(1, -1) * sd
+                round((1 - sum((mns < cint1[2]) + (mns > cint1[1])) / input$sims) * 100, 2)
+            })
+            output$sd2 = renderPrint({
+                cint2 = mn + c(1, -1) * 2 * sd
+                round((1 - sum((mns < cint2[2]) + (mns > cint2[1])) / input$sims) * 100, 2)
+            })
+            output$sd3 = renderPrint({
+                cint3 = mn + c(1, -1) * 3 * sd
+                round((1 - sum((mns < cint3[2]) + (mns > cint3[1])) / input$sims) * 100, 2)
+            })
+        })
+    }
+)
+
---
+++ ui.R
@@ -0,0 +1,37 @@
+shinyUI(fluidPage(
+    titlePanel("Play with the Central Limit Theorem"),
+    sidebarLayout(
+        sidebarPanel(
+            selectInput("var", 
+                        label = "Choose a distribution",
+                        choices = list("Binomial", "Exponential", "Gamma", "Lognormal",
+                            "Normal", "Poisson", "Uniform")),
+            sliderInput("samp", "Select the number of samples", value = 100, min = 25,
+                        max = 1000, step = 25),
+            numericInput("sims", "Number of simulations", value = 1000, min = 25,
+                         max = 10000),
+            checkboxInput("norm", "Draw the Normal distribution", value = TRUE),
+            br(),
+            h5("A way to test the normality of the distribution is by comparing what 
+                proportion of the sample means falls within"),
+            p("1 sd (68.3 normal dist.)"),
+            verbatimTextOutput("sd1"),
+            p("2 sd (95.5 normal dist.)"),
+            verbatimTextOutput("sd2"),
+            p("3 sd (99.7 normal dist.)"),
+            verbatimTextOutput("sd3")
+        ),
+        mainPanel(
+        h4("Definition"),
+        p("In probability theory, the central limit theorem (CLT) states that, given 
+            certain conditions, the arithmetic mean of a sufficiently large number of 
+            iterates of independent random variables, each with a well-defined expected 
+            value and well-defined variance, will be approximately normally distributed, 
+            regardless of the underlying distribution. (source: Wikipedia)"),
+        p("The distribution of the sample means has the mean equal to the one of the 
+              underlying distribution, and the variance equal to the one of the 
+              underlying distribution divided by the square root of the sample rate."),
+        plotOutput('MyPlot')
+        )
+    )
+))
